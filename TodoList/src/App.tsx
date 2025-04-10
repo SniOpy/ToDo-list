3557e76f61c983cdba3aaa694e2c93fa03ad22e6
SniOpy <soufianeibna@gmail.com>
2025-04-10 01:23:29 +0100

    ✨ Adding behaviors

--- TodoList/src/App.tsx
+++ TodoList/src/App.tsx
@@ -1,29 +1,36 @@
+import { useState } from 'react';
+
 function App() {
+  //! State
+  const [tasks, setTasks] = useState([{ name: 'Faire à manger' }, { name: 'Ranger le linge' }]);
+
+  //! comportements
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log('submit');
+  };
+
+  const handleChange = (event) => {
+    setTasks(event.target.value);
+  };
+  //! affichage
   return (
     <>
       <h1>Todo list - Typescript</h1>
 
-      <form action="">
-        <input type="text" placeholder="Ajouter une tâche" />
+      <form action="" onSubmit={handleSubmit}>
+        <input type="text" placeholder="Ajouter une tâche" onChange={handleChange} />
         <button>Ajouter</button>
       </form>
 
       <ul>
-        <li>
-          Faire à manger <button>X</button>
-        </li>
-        <li>
-          Ranger ses affaires <button>X</button>
-        </li>
-        <li>
-          Sortir le chien <button>X</button>
-        </li>
-        <li>
-          Rendez-vous dentiste <button>X</button>
-        </li>
-        <li>
-          Visiter les parents <button>X</button>
-        </li>
+        {tasks.map((task) => {
+          return (
+            <li key={task.name}>
+              {task.name} <button>X</button>
+            </li>
+          );
+        })}
       </ul>
     </>
   );
