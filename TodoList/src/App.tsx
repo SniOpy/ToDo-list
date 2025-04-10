6f97fa5251a350c4bd4018b69ae79f7dcd0dbafc
SniOpy <soufianeibna@gmail.com>
2025-04-10 01:08:00 +0100

    💄  Add layout (structure html)

--- TodoList/src/App.tsx
+++ TodoList/src/App.tsx
@@ -1,5 +1,32 @@
 function App() {
-  return <h1>Typescript</h1>;
+  return (
+    <>
+      <h1>Todo list - Typescript</h1>
+
+      <form action="">
+        <input type="text" placeholder="Ajouter une tâche" />
+        <button>Ajouter</button>
+      </form>
+
+      <ul>
+        <li>
+          Faire à manger <button>X</button>
+        </li>
+        <li>
+          Ranger ses affaires <button>X</button>
+        </li>
+        <li>
+          Sortir le chien <button>X</button>
+        </li>
+        <li>
+          Rendez-vous dentiste <button>X</button>
+        </li>
+        <li>
+          Visiter les parents <button>X</button>
+        </li>
+      </ul>
+    </>
+  );
 }
 
 export default App;
